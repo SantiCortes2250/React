dfddc5539dbe0907dbed5b54aa518fd2c62dcb83
SantiCortes2250 <santicortesrincon15@gmail.com>
2022-03-10 10:32:34 -0500

    Agrego proyecto de rickandmorty

---
+++ react-rick/appreact/src/Componentes/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer } from '../../Layouts/Footer/Footer'
+import { Header } from '../../Layouts/Header/Header'
+import { Main } from '../../Layouts/Main/Main'
+
+
+
+export const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Main/>
+    </div>
+  )
+}
