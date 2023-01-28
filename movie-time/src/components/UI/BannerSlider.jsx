a5e1b915332bea16ed9676c3dc730a71a8e0f2d8
SantiCortes2250 <santicortesrincon15@gmail.com>
2023-01-27 21:33:27 -0500

    new changes in the function onSendData

--- movie-time/src/components/UI/BannerSlider.jsx
+++ movie-time/src/components/UI/BannerSlider.jsx
@@ -1,56 +1,37 @@
-import React, {useState, useEffect} from 'react'
-import { Imagenes } from './Imagenes';
-import {AiFillPlayCircle} from 'react-icons/ai'
-import { getVideoMovie } from '../../api';
-import { VideoMovie } from './VideoMovie';
+import React from "react";
+import { Imagenes } from "./Imagenes";
+import { AiFillPlayCircle } from "react-icons/ai";
 
+export const BannerSlider = ({ item, onSendData }) => {
+  const { id, title, backdrop_path, overview } = item;
 
-export const BannerSlider = ({item, onSendData}) => {
-
-    const {id, title, backdrop_path, overview} = item
-    
-
-    
-   
-
-    const IMAGE_PATH = 'https://image.tmdb.org/t/p/original';
-    const URL_IMAGE = 'https://image.tmdb.org/t/p/original';
-
-    function selectMovie (id) {
-      onSendData(id)
-    };
-    
+ 
+  const URL_IMAGE = "https://image.tmdb.org/t/p/original";
 
   return (
     <>
-    
-    
-      
-      
-
-    
-    <div className='container_banner_movie'>
-        <Imagenes url={`${URL_IMAGE + backdrop_path}`}>
-        </Imagenes>
+      <div className="container_banner_movie">
+        <Imagenes url={`${URL_IMAGE + backdrop_path}`}></Imagenes>
         <div className="container_info_movie">
-            <h1>{title}</h1>
-            <div className="container_stars_years_movie">
-            </div>
-            <div className="description_movie">
-              <p>{overview}</p>
-            </div>
-            <div className="info_movie">
-                
-                
-                <p></p>
-            </div>
-            <button className='btn_watch_movie' onClick={()=>{selectMovie(item.id)}}>
-              <AiFillPlayCircle className='icon_watch'/>
-              <span>Watch Trailer</span>
-            </button>
+          <h1>{title}</h1>
+          <div className="container_stars_years_movie"></div>
+          <div className="description_movie">
+            <p>{overview}</p>
+          </div>
+          <div className="info_movie">
+            <p></p>
+          </div>
+          <button
+            className="btn_watch_movie"
+            onClick={() => {
+              onSendData(id);
+            }}
+          >
+            <AiFillPlayCircle className="icon_watch" />
+            <span>Watch Trailer</span>
+          </button>
         </div>
-    </div>
+      </div>
     </>
-    
-  )
-}
+  );
+};
--- movie-time/src/components/UI/Cards_movie.jsx
+++ movie-time/src/components/UI/Cards_movie.jsx
@@ -1,50 +1,32 @@
-import React, {useState, useEffect} from 'react'
-import { Imagenes } from './Imagenes'
-import {AiFillPlayCircle} from 'react-icons/ai'
-import { getVideoMovie } from '../../api'
+import React from "react";
+import { Imagenes } from "./Imagenes";
+import { AiFillPlayCircle } from "react-icons/ai";
 
 
-
-export const Cards_movie = ({item, onSendData}) => {
-  
-  
-  
-
-  const URL_IMAGE = 'https://image.tmdb.org/t/p/original';
-
-  
-  
-  
-  
-
-
-  function selectMovie (id) {
-    onSendData(id)
-  };
-
-
- 
-
-  
+export const Cards_movie = ({ item, onSendData }) => {
+  const URL_IMAGE = "https://image.tmdb.org/t/p/original";
 
   return (
     <div className="cards_movies">
       <div className="container_img_movie">
         <Imagenes url={`${URL_IMAGE + item.poster_path}`} />
-      </div> 
-      <div className="info_movie_card2"> 
+      </div>
+      <div className="info_movie_card2">
         <div className="btn_wacth2">
-          <AiFillPlayCircle className='icon_btn_watch' onClick={()=>{selectMovie(item.id)}} />
+          <AiFillPlayCircle
+            className="icon_btn_watch"
+            onClick={() => {
+              onSendData(item.id);
+            }}
+          />
         </div>
         <div className="info2">
           <h4>{item.title}</h4>
           <div className="container_p">
             <p>{item.overview}</p>
           </div>
-        </div> 
-      </div> 
+        </div>
+      </div>
     </div>
-  )
-}
-
-
+  );
+};
