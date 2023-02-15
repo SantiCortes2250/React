db7d5b0f4e705659ea5ac0c221a3312a07126167
SantiCortes2250 <santicortesrincon15@gmail.com>
2023-02-14 20:30:54 -0500

    finished project

--- noticies/src/App.js
+++ noticies/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
 import Form from './Components/Form'
 import Header from './Components/Header'
+import Noticies from './Components/Noticies'
 
 
 const App = () => {
+  const [category, setCategory] = useState('')
+  const [data, setdata] = useState([])
+
+  useEffect(() => {
+    
+
+    const callApi = async() => {
+      const API_KEY = '43b16aa23af34def8ac0b82b0a7eeaa2'
+      const URL = `https://newsapi.org/v2/top-headlines?country=US&category=${category}&apiKey=${API_KEY}`
+      const {data} = await axios.get(URL)
+      setdata(data.articles)
+    
+    }
+
+    callApi()
+  }, [category])
+  
+
+
+
+
   return (
     <>
     <Header
@@ -11,7 +34,14 @@ const App = () => {
     />
 
     <div className="container white">
-      <Form/>
+      <Form
+        setCategory={setCategory}
+      />
+
+      <Noticies
+        
+        Noticies={data}
+      />
 
     </div>
     </>
--- noticies/src/Components/Form.js
+++ noticies/src/Components/Form.js
@@ -23,8 +23,6 @@ const Form = ({setCategory}) => {
 
     setCategory(state)
 
-
-
   }
 
   return (
