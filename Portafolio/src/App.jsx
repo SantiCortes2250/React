ffd338bc039e956d107d39dff07c4f21c1461d91
SantiCortes2250 <santicortesrincon15@gmail.com>
2023-04-23 20:14:04 -0500

    update

--- Portafolio/src/App.jsx
+++ Portafolio/src/App.jsx
@@ -1,48 +1,31 @@
-import React from "react"
-import AboutMe from "./Components/AboutMe"
-import Follow from "./Components/Follow"
-import { Header } from "./Components/Header"
-import Projects from "./Components/Projects"
-import Resume from "./Components/Resume"
-import Contact from "./Components/Contact"
-import Footer from "./Components/Footer"
-import Skills from "./Components/Slick/Skills"
-
-
-
-
-
-
-
-
-
-
+import React from "react";
+import AboutMe from "./Components/AboutMe";
+import Follow from "./Components/Follow";
+import { Header } from "./Components/Header";
+import Projects from "./Components/Projects";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Skills from "./Components/Slick/Skills";
 
 function App() {
-  
-
-
   return (
     <>
-    <Follow/>
-    <div className="header">
-      
-      <Header  />
-  
-      
-    </div>
-    <div className="main">
-      <AboutMe/>
-      <Resume/>
-      <Skills/>
-      <Projects/>
-   
-      
-      <Contact/>
-    </div>  
-    <Footer/>
+      <Follow />
+      <div className="header">
+        <Header />
+      </div>
+      <div className="main">
+        <AboutMe />
+        <Resume />
+        <Skills />
+        <Projects />
+
+        <Contact />
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
